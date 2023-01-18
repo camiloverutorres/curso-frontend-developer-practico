445ab074a9881496c4564ceb75a16362d4bb899e
Camilo Verú Torres <camiloverutorres@gmail.com>
2023-01-17 21:00:19 -0500

    Fusión del menú en desktop

---
+++ main.js
@@ -0,0 +1,14 @@
+
+console.log('hola');
+
+
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+function toggleDesktopMenu() {
+
+    desktopMenu.classList.toggle('inactive');
+
+}
+
+menuEmail.addEventListener('click',toggleDesktopMenu)
