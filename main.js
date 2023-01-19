32bf200c3618bab43d624aff3369813bfe1bf63f
Camilo Verú Torres <camiloverutorres@gmail.com>
2023-01-18 22:11:16 -0500

    carrito de compras

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 
 console.log('hola');
 
-
 /*Fusión del menú en desktop*/
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
 function toggleDesktopMenu() {
+
+    if (!shopMenu.classList.toggle('inactive')) {
+
+        shopMenu.classList.add('inactive');
+    }
 
     desktopMenu.classList.toggle('inactive');
 
@@ -20,13 +24,40 @@ menuEmail.addEventListener('click',toggleDesktopMenu)
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+
+
 function toggleMobilemenu() {
 
-    mobileMenu.classList.toggle('inactive');
+    if (!shopMenu.classList.toggle('inactive')) {
 
+        shopMenu.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
 }
 
 burgerMenu.addEventListener('click',toggleMobilemenu);
 
+/*carrito de compras*/
 
+const shopIcon = document.querySelector('.navbar-shopping-cart');
+const shopMenu = document.querySelector('.product-detail');
+
+function toggleShopMenu() {
+
+    if (!mobileMenu.classList.toggle('inactive')) {
+
+        mobileMenu.classList.add('inactive');
+    }
+
+     shopMenu.classList.toggle('inactive');
+}
+
+
+shopIcon.addEventListener('click',toggleShopMenu);
+
+/* cuando le click al shopicon y el mobilemenu está abierto o ¡inactive else add inactive /*
+cuando le clink al burgeicon y el si shopmenu está abierto o ¡inactive else add inactive
+
+*/
 
